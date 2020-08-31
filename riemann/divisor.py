29b50db2dd3417bb213f975cfc85ef33e53179b4
Jeremy Kun <j2kun@users.noreply.github.com>
2020-08-31 16:24:17 -0700

    divisor: improve error message

--- riemann/divisor.py
+++ riemann/divisor.py
@@ -3,6 +3,8 @@
 def divisor_sum(n: int) -> int:
     '''Compute the sum of divisors of a positive integer.'''
     if n <= 0:
-        raise ValueError("Non-positive numbers are not supported.")
+        raise ValueError(
+            "Non-positive number {} is not supported.".format(n)
+        )
 
     return sum(i for i in range(1, n+1) if n%i == 0)
