dd4659278eb9dfa8e912196ed7a14ac75f330490
Jeremy Kun <j2kun@users.noreply.github.com>
2020-08-31 16:06:37 -0700

    divisor: implement slow version

--- riemann/divisor.py
+++ riemann/divisor.py
@@ -1,4 +1,7 @@
 '''Compute the sum of divisors of a number.'''
 
 def divisor_sum(n: int) -> int:
-    raise ValueError("Not implemented.")
+    if n <= 0:
+        raise ValueError("Non-positive numbers are not supported.")
+
+    return sum(i for i in range(1, n+1) if n%i == 0)
