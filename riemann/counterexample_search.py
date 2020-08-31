43ca55c9acba6118cab42579c7ff07a12d85b8c1
Jeremy Kun <j2kun@users.noreply.github.com>
2020-08-31 16:28:41 -0700

    counterexample_search: extract SEARCH_START to a constant

--- riemann/counterexample_search.py
+++ riemann/counterexample_search.py
@@ -3,12 +3,15 @@ import math
 from riemann.divisor import divisor_sum
 
 
+SEARCH_START = 5041
+
+
 def witness_value(n: int) -> float:
     denominator = n * math.log(math.log(n))
     return divisor_sum(n) / denominator
 
 
-def search(max_range: int, search_start:int = 5041) -> int:
+def search(max_range: int, search_start: int = SEARCH_START) -> int:
     '''Search for a counterexample to the Riemann Hypothesis.'''
     for n in range(search_start, max_range + 1):
         if witness_value(n) > 1.782:
