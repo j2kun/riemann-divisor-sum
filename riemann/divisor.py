ae535ec29fa76f014bd8c92fc265329668566be8
Jeremy Kun <j2kun@users.noreply.github.com>
2020-08-31 21:47:00 -0700

    divisor: go back to simpler scheme

--- riemann/divisor.py
+++ riemann/divisor.py
@@ -1,37 +1,22 @@
 '''Compute the sum of divisors of a number.'''
-from functools import lru_cache
+import math
+from numba import njit
 
 
-@lru_cache(maxsize=1000000)
+@njit
 def divisor_sum(n: int) -> int:
-    '''Compute the sum of divisors of a positive integer.
-
-    Algorithm is described https://math.stackexchange.com/a/22744
-    '''
+    '''Compute the sum of divisors of a positive integer.'''
     if n <= 0:
-        raise ValueError(
-            "Non-positive number {} is not supported.".format(n)
-        )
+        return 0
 
-    sign_sequence = [1, 1, -1, -1]
-
+    i = 1
+    limit = math.sqrt(n)
     the_sum = 0
-    i = 0
-    while True:
-        sign = sign_sequence[i % 4]
-        x = i // 2 + 1
-        if i % 2 == 1:
-            x = -x
-        arg_diff = int((3 * x * x - x) / 2)
-
-        if n - arg_diff < 0:
-           break
-
-        if n - arg_diff == 0:
-           the_sum += sign * n
-           break
-
-        the_sum += sign * divisor_sum(n - arg_diff)
+    while i <= limit:
+        if n % i == 0:
+            the_sum += i
+            if i != limit:
+                the_sum += n // i
         i += 1
 
     return the_sum
--- test/divisor_test.py
+++ test/divisor_test.py
@@ -19,11 +19,3 @@ def test_sum_of_divisors_of_1():
 
 def test_sum_of_divisors_of_72():
     assert 195 == divisor_sum(72)
-
-def test_illegal_input_0():
-    with pytest.raises(ValueError):
-        divisor_sum(0)
-
-def test_illegal_input_negative():
-    with pytest.raises(ValueError):
-        divisor_sum(-5)
