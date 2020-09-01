9c1332450c083265774fcb4404a2f0e9e93190b0
Jeremy Kun <j2kun@users.noreply.github.com>
2020-08-31 17:07:36 -0700

    divisor_test: test first few dozen integers

--- test/divisor_test.py
+++ test/divisor_test.py
@@ -1,6 +1,19 @@
 import pytest
 from riemann.divisor import divisor_sum
 
+# sigma(n), starting at n=1
+divisor_sums = [
+1, 3, 4, 7, 6, 12, 8, 15, 13, 18, 12, 28, 14, 24, 24, 31, 18, 39, 20, 42, 32,
+36, 24, 60, 31, 42, 40, 56, 30, 72, 32, 63, 48, 54, 48, 91, 38, 60, 56, 90, 42,
+96, 44, 84, 78, 72, 48, 124, 57, 93, 72
+]
+
+input_output_pairs = zip(range(1, len(divisor_sums) + 1), divisor_sums)
+
+@pytest.mark.parametrize("test_input,expected", input_output_pairs)
+def test_sum_of_divisors(test_input, expected):
+    assert divisor_sum(test_input) == expected
+
 def test_sum_of_divisors_of_1():
     assert 1 == divisor_sum(1)
 
