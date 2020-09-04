cdda1686d11a5083ea88f52f6e3aa1e4923a4f2a
Jeremy Kun <j2kun@users.noreply.github.com>
2020-09-03 22:11:31 -0700

    add database interface

---
+++ riemann/database.py
@@ -0,0 +1,36 @@
+'''An interface for a database containing divisor sums.'''
+
+from abc import ABC
+from abc import abstractmethod
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class RiemannDivisorSum:
+    n: int
+    divisor_sum: int
+    witness_value: float
+
+
+@dataclass
+class SummaryStats:
+    largest_computed_n: RiemannDivisorSum
+    largest_witness_value: RiemannDivisorSum
+
+
+class DivisorDb(ABC):
+    @abstractmethod
+    def load() -> List[RiemannDivisorSum]:
+        '''Load the entire database.'''
+        pass
+
+    @abstractmethod
+    def upsert(data: List[RiemannDivisorSum]) -> None:
+        '''Insert or update the given list of data points.'''
+        pass
+
+    @abstractmethod
+    def summarize() -> SummaryStats:
+        '''Summarize the contents of the database.'''
+        pass
