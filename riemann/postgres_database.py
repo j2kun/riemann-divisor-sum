ef6fd38d0885b08863956afded9e0245e5224362
Jeremy Kun <j2kun@users.noreply.github.com>
2020-10-06 00:45:10 -0700

    add postgres database implementation

--- riemann/postgres_database.py
+++ riemann/postgres_database.py
@@ -1,4 +1,5 @@
 from dataclasses import asdict
+from gmpy2 import mpz
 from riemann.database import DivisorDb
 from riemann.database import SearchMetadataDb
 from riemann.types import RiemannDivisorSum
@@ -8,22 +9,32 @@ from riemann.types import deserialize_search_state
 from typing import List
 from typing import Union
 import psycopg2
+import psycopg2.extras
+
+DEFAULT_DATA_SOURCE_NAME = 'dbname=divisor'
 
 
 class PostgresDivisorDb(DivisorDb, SearchMetadataDb):
-    def __init__(self, data_source_name=None):
+    def __init__(self, data_source_name=None, data_source_dict=None):
         '''Create a database connection.'''
-        if data_source_name is None:
-            raise ValueError("Must specify a dsn, such as 'dbname=divisor'")
-        self.connection = psycopg2.connect(data_source_name)
+        if data_source_name is None and data_source_dict is None:
+            data_source_name = DEFAULT_DATA_SOURCE_NAME
+
+        if data_source_dict is not None:
+            self.connection = psycopg2.connect(**data_source_dict)
+        else:
+            self.connection = psycopg2.connect(data_source_name)
 
     def initialize_schema(self):
         cursor = self.connection.cursor()
         cursor.execute('''
+        CREATE EXTENSION IF NOT EXISTS pgmp;
+        ''')
+        cursor.execute('''
         CREATE TABLE IF NOT EXISTS RiemannDivisorSums (
-            n UNSIGNED BIG INT CONSTRAINT divisor_sum_pk PRIMARY KEY,
-            divisor_sum UNSIGNED BIG INT,
-            witness_value REAL
+            n mpz CONSTRAINT divisor_sum_pk PRIMARY KEY,
+            divisor_sum mpz,
+            witness_value double precision
         );''')
         cursor.execute('''
         CREATE TABLE IF NOT EXISTS SearchMetadata (
@@ -37,8 +48,8 @@ class PostgresDivisorDb(DivisorDb, SearchMetadataDb):
 
     def convert_records(self, rows):
         return [
-            RiemannDivisorSum(n=row[0],
-                              divisor_sum=row[1],
+            RiemannDivisorSum(n=mpz(row[0]),
+                              divisor_sum=mpz(row[1]),
                               witness_value=row[2]) for row in rows
         ]
 
@@ -56,13 +67,18 @@ class PostgresDivisorDb(DivisorDb, SearchMetadataDb):
         query = '''
         INSERT INTO
             RiemannDivisorSums(n, divisor_sum, witness_value)
-            VALUES(:n, :divisor_sum, :witness_value)
+            VALUES %s
         ON CONFLICT(n) DO UPDATE SET
             divisor_sum=excluded.divisor_sum,
             witness_value=excluded.witness_value;
         '''
+        template = "(%(n)s, %(divisor_sum)s, %(witness_value)s)"
 
-        cursor.executemany(query, [asdict(record) for record in records])
+        psycopg2.extras.execute_values(
+            cur=cursor,
+            sql=query,
+            argslist=[asdict(record) for record in records],
+            template=template)
         self.connection.commit()
 
     def summarize(self) -> SummaryStats:
@@ -79,21 +95,18 @@ class PostgresDivisorDb(DivisorDb, SearchMetadataDb):
             return SummaryStats(largest_computed_n=None,
                                 largest_witness_value=None)
 
-        cursor.execute(
-            '''
+        cursor.execute('''
             SELECT
                 n, divisor_sum, witness_value
             FROM RiemannDivisorSums
             WHERE
-                n = :max_n
-                OR witness_value = :max_witness;
-        ''', {
-                'max_n': max_n,
-                'max_witness': max_witness
-            })
+                n = '%s'::mpz
+                OR witness_value = %s
+            ;
+        ''' % (max_n, max_witness))
 
         records = self.convert_records(cursor.fetchall())
-        largest_n_record = [r for r in records if r.n == max_n][0]
+        largest_n_record = [r for r in records if r.n == mpz(max_n)][0]
         largest_witness_record = [
             r for r in records if r.witness_value == max_witness
         ][0]
@@ -104,8 +117,7 @@ class PostgresDivisorDb(DivisorDb, SearchMetadataDb):
     def latest_search_metadata(
             self, search_state_type: str) -> Union[SearchMetadata, None]:
         cursor = self.connection.cursor()
-        cursor.execute(
-            '''
+        cursor.execute('''
             SELECT
               start_time,
               end_time,
@@ -113,10 +125,10 @@ class PostgresDivisorDb(DivisorDb, SearchMetadataDb):
               starting_search_state,
               ending_search_state
             FROM SearchMetadata
-            WHERE search_state_type = :search_state_type
+            WHERE search_state_type = '%s'
             ORDER BY end_time DESC
             LIMIT 1;
-        ''', {'search_state_type': search_state_type})
+        ''' % search_state_type)
 
         row = cursor.fetchone()
         if not row:
@@ -143,25 +155,14 @@ class PostgresDivisorDb(DivisorDb, SearchMetadataDb):
               starting_search_state,
               ending_search_state
             )
-            VALUES(
-                :start_time,
-
-                :end_time,
-                :search_state_type,
-                :starting_search_state,
-                :ending_search_state)
+            VALUES (%s, %s, %s, %s, %s)
         '''
 
         cursor.execute(
-            query, {
-                'start_time': metadata.start_time,
-                'end_time': metadata.end_time,
-                'search_state_type': metadata.search_state_type,
-                'starting_search_state':
-                metadata.starting_search_state.serialize(),
-                'ending_search_state':
-                metadata.ending_search_state.serialize(),
-            })
+            cursor.mogrify(query, (metadata.start_time, metadata.end_time,
+                                   metadata.search_state_type,
+                                   metadata.starting_search_state.serialize(),
+                                   metadata.ending_search_state.serialize())))
         self.connection.commit()
 
 
@@ -170,6 +171,10 @@ if __name__ == "__main__":
     # dsn is a string that specifies the database name
     # and any additional config,
     # e.g., 'dbname=divisor'
-    db = PostgresDivisorDb(data_source_name=sys.argv[1])
+    data_source_name = None
+    if len(sys.argv) == 2:
+        data_source_name = sys.argv[1]
+
+    db = PostgresDivisorDb(data_source_name=data_source_name)
     db.initialize_schema()
     db.connection.close()
