a986c29cbc78bf5204ddadc30b3d188a123d22c6
Jeremy Kun <j2kun@users.noreply.github.com>
2020-12-27 09:58:48 -0800

    add docker memory flag to deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ set -euf -o pipefail
 docker build -t divisordb -f divisordb.Dockerfile .
 docker build -t divisorsearch -f divisorsearch.Dockerfile .
 
-docker run -d --name divisordb -p 5432:5432 divisordb:latest
+docker run -d --name divisordb -p 5432:5432 --memory="1G" divisordb:latest
 
 # The host address for the divisordb container is nested inside a json
 # `jq` is a CLI for stream processing json data.
 export PGHOST=$(docker network inspect bridge | jq -r '.[0].Containers[] | select(.Name=="divisordb") | .IPv4Address' | sed 's|/.*$||g')
 
-docker run -d --name divisorsearch --env PGHOST="$PGHOST" divisorsearch:latest
+docker run -d --name divisorsearch --env PGHOST="$PGHOST" --memory="15G" divisorsearch:latest
