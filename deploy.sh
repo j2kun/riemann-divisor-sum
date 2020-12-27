07936d0ae78865e1dfe96aac61d5509ffec5a3ab
Jeremy Kun <j2kun@users.noreply.github.com>
2020-12-27 10:07:39 -0800

    deploy: add 5 second sleep before divisorsearch run
    This ensures that the divisordb container has enough time to start up

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,6 @@ docker run -d --name divisordb -p 5432:5432 --memory="1G" divisordb:latest
 # `jq` is a CLI for stream processing json data.
 export PGHOST=$(docker network inspect bridge | jq -r '.[0].Containers[] | select(.Name=="divisordb") | .IPv4Address' | sed 's|/.*$||g')
 
+# give time for the divisordb container to start up
+sleep 5
 docker run -d --name divisorsearch --env PGHOST="$PGHOST" --memory="15G" divisorsearch:latest
