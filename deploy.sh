18e7a3543113995780cf7e4de01fbd2e847ca7b9
Jeremy Kun <j2kun@users.noreply.github.com>
2020-12-27 21:27:29 -0800

    deploy.sh: simplify ip address lookup

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,7 @@ docker build -t divisorsearch -f divisorsearch.Dockerfile .
 
 docker run -d --name divisordb -p 5432:5432 --memory="1G" divisordb:latest
 
-# The host address for the divisordb container is nested inside a json
-# `jq` is a CLI for stream processing json data.
-export PGHOST=$(docker network inspect bridge | jq -r '.[0].Containers[] | select(.Name=="divisordb") | .IPv4Address' | sed 's|/.*$||g')
+export PGHOST=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" divisordb)
 
 # give time for the divisordb container to start up
 sleep 5
