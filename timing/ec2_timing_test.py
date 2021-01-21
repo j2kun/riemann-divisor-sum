d0e40ecc6c48f592d09312bf91934e2c5f70e897
Jeremy Kun <j2kun@users.noreply.github.com>
2021-01-21 07:46:58 -0800

    change timing test to test database upsert

--- timing/ec2_timing_test.py
+++ timing/ec2_timing_test.py
@@ -3,37 +3,34 @@ from riemann import superabundant
 from riemann.search_strategy import SuperabundantSearchStrategy
 from riemann.superabundant import partition_to_prime_factorization
 from riemann.superabundant import partitions_of_n
+from riemann.postgres_database import PostgresDivisorDb
 from riemann.types import SearchState
 from riemann.types import SuperabundantEnumerationIndex
 import cProfile
 import numba
 import time
 
+db = PostgresDivisorDb(data_source_name='')
 
-# allow numba to compile
-partition_to_prime_factorization(partitions_of_n(4)[0])
 samples = 5
+search_strategy = SuperabundantSearchStrategy().starting_from(
+    SuperabundantEnumerationIndex(71, 196047))
+batch = search_strategy.next_batch(250000)
 
-
-def run_test(search_state_start, batch_size):
-    search_strategy = SuperabundantSearchStrategy().starting_from(
-        search_state_start)
-
+def run_test(batch):
     times = []
     for i in range(samples):
         print(f'Running sample {i}')
         start = time.time()
-        search_strategy.next_batch(batch_size)
+        db.upsert(batch)
         end = time.time()
         print(end - start)
-        times.append(end-start)
+        times.append(end - start)
 
     return sum(times) / samples
 
 
-print(run_test(SuperabundantEnumerationIndex(71, 196047), 250000))
-
+print(run_test(batch))
 
 print("------- cprofile -------")
-cProfile.runctx(
-    'run_test(SuperabundantEnumerationIndex(71, 196047), 250000)', globals(), locals())
+cProfile.runctx('run_test(batch)', globals(), locals())
