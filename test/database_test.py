15c68229927f6748dd6d29eb9f4ebaaa73c81f88
Jeremy Kun <j2kun@users.noreply.github.com>
2021-01-24 12:52:28 -0800

    rewrite tests for new database interface

--- test/database_test.py
+++ test/database_test.py
@@ -1,15 +1,18 @@
+from dataclasses import replace
 from datetime import datetime
-
+from gmpy2 import mpz
 import pytest
 import testing.postgresql
-from gmpy2 import mpz
+
 from riemann.database import DivisorDb
 from riemann.in_memory_database import InMemoryDivisorDb
 from riemann.postgres_database import PostgresDivisorDb
 from riemann.types import ExhaustiveSearchIndex
 from riemann.types import RiemannDivisorSum
+from riemann.types import SearchBlockState
 from riemann.types import SearchMetadata
 from riemann.types import SummaryStats
+from riemann.types import SuperabundantEnumerationIndex
 
 
 def noop_teardown():
@@ -37,42 +40,93 @@ def db(request):
     db.teardown()
 
 
-@pytest.mark.parametrize('db', [createInMemoryDb, createPostgresDb], indirect=True)
+DATABASES = [
+    createInMemoryDb,
+    # createPostgresDb,
+]
+
+
+@pytest.mark.parametrize('db', DATABASES, indirect=True)
 class TestDatabase:
+    def populate_search_blocks(self, db):
+        metadatas = [
+            SearchMetadata(
+                search_index_type='ExhaustiveSearchIndex',
+                starting_search_index=ExhaustiveSearchIndex(n=i),
+                ending_search_index=ExhaustiveSearchIndex(n=i+1))
+            for i in range(1, 10, 2)]
+        db.insert_search_blocks(metadatas)
+
     def test_initially_empty(self, db):
         assert len(db.load()) == 0
+        with pytest.raises(ValueError):
+            db.claim_next_search_block(
+                search_index_type='ExhaustiveSearchIndex')
 
-    def test_upsert_from_empty(self, db):
+    def test_insert_search_block(self, db):
+        metadata = SearchMetadata(
+            search_index_type='ExhaustiveSearchIndex',
+            starting_search_index=ExhaustiveSearchIndex(n=1),
+            ending_search_index=ExhaustiveSearchIndex(n=2),
+        )
+        db.insert_search_blocks([metadata])
+        actual = db.claim_next_search_block(
+            search_index_type='ExhaustiveSearchIndex')
+        assert actual == replace(metadata, state=SearchBlockState.IN_PROGRESS)
+
+        # no remaining unstarted blocks
+        with pytest.raises(ValueError):
+            db.claim_next_search_block(
+                search_index_type='ExhaustiveSearchIndex')
+
+    def test_claim_multiple_blocks(self, db):
+        self.populate_search_blocks(db)
+        block = db.claim_next_search_block(
+            search_index_type='ExhaustiveSearchIndex')
+        next_block = db.claim_next_search_block(
+            search_index_type='ExhaustiveSearchIndex')
+        assert block != next_block
+
+    def test_claim_and_finish_search_block(self, db):
+        self.populate_search_blocks(db)
+        block = db.claim_next_search_block(
+            search_index_type='ExhaustiveSearchIndex')
+
         records = [
             RiemannDivisorSum(n=1, divisor_sum=1, witness_value=1),
             RiemannDivisorSum(n=2, divisor_sum=2, witness_value=2),
         ]
 
-        db.insert(records)
+        db.finish_search_block(block, records)
         assert set(db.load()) == set(records)
 
-    def test_upsert_mpz(self, db):
+        next_block = db.claim_next_search_block(
+            search_index_type='ExhaustiveSearchIndex')
+        assert block != next_block
+
+
+    def test_search_block_with_mpz_values(self, db):
+        metadatas = [
+            SearchMetadata(
+                search_index_type='SuperabundantEnumerationIndex',
+                starting_search_index=SuperabundantEnumerationIndex(
+                    level=i, index_in_level=0),
+                ending_search_index=SuperabundantEnumerationIndex(level=i+1, index_in_level=0))
+            for i in range(1, 10, 2)]
+        db.insert_search_blocks(metadatas)
+
+        block = db.claim_next_search_block(
+            search_index_type='SuperabundantEnumerationIndex')
+
         records = [
             RiemannDivisorSum(n=mpz(1), divisor_sum=mpz(1), witness_value=1),
+            RiemannDivisorSum(n=mpz(2), divisor_sum=mpz(2), witness_value=2),
         ]
 
-        db.insert(records)
-        assert set(db.load()) == set(records)
-
-    def test_upsert_from_nonempty(self, db):
-        records = [
-            RiemannDivisorSum(n=1, divisor_sum=1, witness_value=1),
-            RiemannDivisorSum(n=2, divisor_sum=2, witness_value=2),
-        ]
-        db.insert(records)
-
-        new_records = [
-            RiemannDivisorSum(n=3, divisor_sum=3, witness_value=3),
-            RiemannDivisorSum(n=4, divisor_sum=4, witness_value=4),
-        ]
-        db.insert(new_records)
-
-        assert set(db.load()) == set(records + new_records)
+        db.finish_search_block(block, records)
+        stored = db.load()
+        assert len(stored) == 1
+        assert stored[0].n == 2
 
     def test_summarize_empty(self, db):
         expected = SummaryStats(largest_computed_n=None,
@@ -81,57 +135,15 @@ class TestDatabase:
         assert expected == db.summarize()
 
     def test_summarize_nonempty(self, db):
+        self.populate_search_blocks(db)
+        block = db.claim_next_search_block(
+            search_index_type='ExhaustiveSearchIndex')
         records = [
             RiemannDivisorSum(n=9, divisor_sum=3, witness_value=3),
             RiemannDivisorSum(n=4, divisor_sum=4, witness_value=4),
         ]
-        db.insert(records)
+        db.finish_search_block(block, records)
         expected = SummaryStats(largest_computed_n=records[0],
                                 largest_witness_value=records[1])
 
         assert expected == db.summarize()
-
-    def test_fetch_empty_metadata(self, db):
-        name = "ExhaustiveSearchIndex"
-        assert db.latest_search_metadata(name) is None
-
-    def test_store_metadata(self, db):
-        name = "ExhaustiveSearchIndex"
-        metadata = [
-            SearchMetadata(start_time=datetime(2020, 9, 1, 0, 0, 0),
-                           end_time=datetime(2020, 9, 1, 1, 0, 0),
-                           search_index_type=name,
-                           starting_search_index=ExhaustiveSearchIndex(n=1),
-                           ending_search_index=ExhaustiveSearchIndex(n=2)),
-        ]
-
-        db.insert_search_metadata(metadata[0])
-        assert (
-            metadata[0].starting_search_index
-            == db.latest_search_metadata(name).starting_search_index
-        )
-
-    def test_store_metadata_ordering(self, db):
-        name = "ExhaustiveSearchIndex"
-        older_time = datetime(2016, 9, 1, 1, 0, 0)
-        newer_time = datetime(2020, 9, 1, 1, 0, 0)
-        metadata = [
-            SearchMetadata(start_time=older_time,
-                           end_time=older_time,
-                           search_index_type=name,
-                           starting_search_index=ExhaustiveSearchIndex(n=1),
-                           ending_search_index=ExhaustiveSearchIndex(n=2)),
-            SearchMetadata(start_time=newer_time,
-                           end_time=newer_time,
-                           search_index_type=name,
-                           starting_search_index=ExhaustiveSearchIndex(n=1),
-                           ending_search_index=ExhaustiveSearchIndex(n=2)),
-        ]
-
-        # insert the newer one first
-        db.insert_search_metadata(metadata[1])
-        db.insert_search_metadata(metadata[0])
-        assert (
-            metadata[1].starting_search_index
-            == db.latest_search_metadata(name).starting_search_index
-        )
