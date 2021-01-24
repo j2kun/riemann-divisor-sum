b470f2dc4d3d2697def6bfe6110ba26bd02d63f1
Jeremy Kun <j2kun@users.noreply.github.com>
2021-01-24 14:51:23 -0800

    update search_strategy_test for new interface
    I also added one more test specific to the Superabundant search, which
    relates to the occurence where a block's ending index lands exactly on
    the boundary of a level.

--- test/search_strategy_test.py
+++ test/search_strategy_test.py
@@ -14,9 +14,10 @@ from riemann.search_strategy import SuperabundantSearchStrategy
 def test_search_strategy_uninitialized(make_strategy, expected_numbers_1,
                                        expected_numbers_2):
     search = make_strategy()
-    searched_numbers = set([x.n for x in search.next_batch(4)])
+    blocks = search.generate_search_blocks(count=2, batch_size=4)
+    searched_numbers = set([x.n for x in search.process_block(blocks[0])])
     assert searched_numbers == set(expected_numbers_1)
-    searched_numbers = set([x.n for x in search.next_batch(4)])
+    searched_numbers = set([x.n for x in search.process_block(blocks[1])])
     assert searched_numbers == set(expected_numbers_2)
 
 
@@ -29,26 +30,9 @@ def test_search_strategy_uninitialized(make_strategy, expected_numbers_1,
 def test_search_strategy_initialized(make_strategy, search_index,
                                      expected_numbers):
     search = make_strategy().starting_from(search_index)
-    searched_numbers = set([x.n for x in search.next_batch(2)])
+    blocks = search.generate_search_blocks(count=1, batch_size=2)
+    searched_numbers = set([x.n for x in search.process_block(blocks[0])])
     assert searched_numbers == set(expected_numbers)
-
-
-@pytest.mark.parametrize(
-    'make_strategy,search_index,expected_ending_index',
-    [(ExhaustiveSearchStrategy, ExhaustiveSearchIndex(n=100),
-      ExhaustiveSearchIndex(n=102)),
-     (SuperabundantSearchStrategy,
-      SuperabundantEnumerationIndex(level=5, index_in_level=1),
-      SuperabundantEnumerationIndex(level=5, index_in_level=3)),
-     (SuperabundantSearchStrategy,
-      SuperabundantEnumerationIndex(level=4, index_in_level=4),
-      SuperabundantEnumerationIndex(level=5, index_in_level=1))])
-def test_search_strategy_index(make_strategy, search_index,
-                               expected_ending_index):
-    search = make_strategy().starting_from(search_index)
-    assert search.search_index() == search_index
-    search.next_batch(2)
-    assert search.search_index() == expected_ending_index
 
 
 @pytest.mark.parametrize(
@@ -58,6 +42,31 @@ def test_search_strategy_index(make_strategy, search_index,
       SuperabundantEnumerationIndex(level=5, index_in_level=1))])
 def test_search_index_reset(make_strategy, search_index):
     search = make_strategy().starting_from(search_index)
-    batch = search.next_batch(2)
-    batch_2 = search.starting_from(search_index).next_batch(2)
-    assert batch == batch_2
+    blocks = search.generate_search_blocks(count=2, batch_size=2)
+    blocks_2 = search.starting_from(
+        search_index).generate_search_blocks(count=2, batch_size=2)
+
+    assert blocks[0].starting_search_index == blocks_2[0].starting_search_index
+    assert blocks[1].starting_search_index == blocks_2[1].starting_search_index
+
+
+def test_superabundant_exactly_landing_on_level_boundary():
+    search = SuperabundantSearchStrategy().starting_from(
+        SuperabundantEnumerationIndex(level=4, index_in_level=1))
+    '''
+    The parititions of 4 are 
+
+    [[4], [3, 1], [2, 2], [2, 1, 1], [1, 1, 1, 1]]
+
+    So if the index in the level is 1, a batch size of 4 should land
+    exactly on the boundary of the level.
+    '''
+    blocks = search.generate_search_blocks(count=2, batch_size=4)
+    assert blocks[0].starting_search_index == SuperabundantEnumerationIndex(
+        level=4, index_in_level=1)
+    assert blocks[0].ending_search_index == SuperabundantEnumerationIndex(
+        level=4, index_in_level=4)
+    assert blocks[1].starting_search_index == SuperabundantEnumerationIndex(
+        level=5, index_in_level=0)
+    assert blocks[1].ending_search_index == SuperabundantEnumerationIndex(
+        level=5, index_in_level=3)
