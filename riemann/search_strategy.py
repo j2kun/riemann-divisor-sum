c93ef55f2b410b2cd03c353506cf050ac80028a9
Jeremy Kun <j2kun@users.noreply.github.com>
2021-01-24 14:13:19 -0800

    implement new search_strategy interface

--- riemann/search_strategy.py
+++ riemann/search_strategy.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 
 from abc import ABC
 from abc import abstractmethod
+from copy import deepcopy
+from typing import Iterator
 from typing import List
 
 from riemann import divisor
@@ -13,6 +15,7 @@ from riemann.superabundant import partition_to_prime_factorization
 from riemann.types import ExhaustiveSearchIndex
 from riemann.types import RiemannDivisorSum
 from riemann.types import SearchIndex
+from riemann.types import SearchMetadata
 from riemann.types import SuperabundantEnumerationIndex
 
 
@@ -49,23 +52,30 @@ class ExhaustiveSearchStrategy(SearchStrategy):
     '''A search strategy that tries every positive integer starting from 5041.'''
 
     def __init__(self):
-        self._search_index = 5041
+        self._search_index = ExhaustiveSearchIndex(n=5041)
 
     def starting_from(
             self,
             search_index: ExhaustiveSearchIndex) -> ExhaustiveSearchStrategy:
-        self._search_index = search_index.n
+        self._search_index = search_index
         return self
 
-    def search_index(self) -> ExhaustiveSearchIndex:
-        return ExhaustiveSearchIndex(n=self._search_index)
+    def generate_search_blocks(self, count: int, batch_size: int) -> List[SearchMetadata]:
+        start = self._search_index.n
+        blocks = []
+        for i in range(count):
+            starting_index = ExhaustiveSearchIndex(n=start)
+            ending_index = ExhaustiveSearchIndex(n=start+batch_size-1)
+            start += batch_size
+            blocks.append(SearchMetadata(
+                starting_search_index=starting_index,
+                ending_search_index=ending_index,
+                search_index_type='ExhaustiveSearchIndex',
+            ))
+        return blocks
 
-    def next_batch(self, batch_size: int) -> List[RiemannDivisorSum]:
-        ending_n = self._search_index + batch_size - 1
-        sums = divisor.compute_riemann_divisor_sums(self._search_index,
-                                                    ending_n)
-        self._search_index = ending_n + 1
-        return sums
+    def process_block(self, block: SearchMetadata) -> List[RiemannDivisorSum]:
+        return divisor.compute_riemann_divisor_sums(block.starting_search_index.n, block.ending_search_index.n)
 
 
 class SuperabundantSearchStrategy(SearchStrategy):
@@ -73,7 +83,7 @@ class SuperabundantSearchStrategy(SearchStrategy):
 
     def __init__(self):
         self._search_index = SuperabundantEnumerationIndex(level=1,
-                                                          index_in_level=0)
+                                                           index_in_level=0)
         self.current_level = [[1]]
         self.__maybe_reset_current_level__()
 
@@ -84,34 +94,64 @@ class SuperabundantSearchStrategy(SearchStrategy):
         self.__maybe_reset_current_level__()
         return self
 
-    def search_index(self) -> SuperabundantEnumerationIndex:
-        return self._search_index
+    def generate_search_blocks(self, count: int, batch_size: int) -> List[SearchMetadata]:
+        return [self.generate_next_block(batch_size) for i in range(count)]
 
-    def next_batch(self, batch_size: int) -> List[RiemannDivisorSum]:
-        sums = []
+    def generate_next_block(self, batch_size: int) -> SearchMetadata:
+        starting_index = deepcopy(self._search_index)
+
         '''
-        Because a batch can cross multiple levels, we need to possibly split
-        the batch into pieces.
+        Because a block can cross multiple levels, we need to possibly split
+        the block into pieces.
         '''
         ending_level_index = self._search_index.index_in_level + batch_size - 1
         while ending_level_index >= len(self.current_level):
-            for i in range(self._search_index.index_in_level,
-                           len(self.current_level)):
-                fac = partition_to_prime_factorization(self.current_level[i])
-                sums.append(superabundant.compute_riemann_divisor_sum(fac))
-
             ending_level_index -= len(self.current_level)
             self._search_index = SuperabundantEnumerationIndex(
-                level=self._search_index.level + 1, index_in_level=0)
+                level=self._search_index.level+1, index_in_level=0)
             self.__maybe_reset_current_level__()  # definitely
 
-        for i in range(self._search_index.index_in_level,
-                       ending_level_index + 1):
-            fac = partition_to_prime_factorization(self.current_level[i])
-            sums.append(superabundant.compute_riemann_divisor_sum(fac))
+        ending_index = SuperabundantEnumerationIndex(
+            level=self._search_index.level,
+            index_in_level=ending_level_index)
+
+        if (ending_level_index == len(self.current_level) - 1):
+            self._search_index = SuperabundantEnumerationIndex(
+                level=self._search_index.level+1,
+                index_in_level=0)
+            self.__maybe_reset_current_level__()  # definitely
+        else:
             self._search_index = SuperabundantEnumerationIndex(
                 level=self._search_index.level,
-                index_in_level=self._search_index.index_in_level + 1)
+                index_in_level=ending_level_index+1)
+
+        return SearchMetadata(
+            starting_search_index=starting_index,
+            ending_search_index=ending_index,
+            search_index_type='SuperabundantEnumerationIndex',
+        )
+
+    def process_block(self, block: SearchMetadata) -> List[RiemannDivisorSum]:
+        sums = []
+        current_index = block.starting_search_index
+        ending_index = block.ending_search_index
+        current_level = CachedPartitionsOfN(current_index.level)
+
+        while current_index.level < ending_index.level:
+            for i in range(current_index.index_in_level, len(current_level)):
+                fac = partition_to_prime_factorization(current_level[i])
+                sums.append(superabundant.compute_riemann_divisor_sum(fac))
+
+            current_index = SuperabundantEnumerationIndex(
+                level=current_index.level+1,
+                index_in_level=0,
+            )
+            current_level = CachedPartitionsOfN(current_index.level)
+
+        for i in range(current_index.index_in_level,
+                       ending_index.index_in_level+1):
+            fac = partition_to_prime_factorization(current_level[i])
+            sums.append(superabundant.compute_riemann_divisor_sum(fac))
 
         return sums
 
