34330f05385564ef68811b46d5f6aca5519c06ef
Jeremy Kun <j2kun@users.noreply.github.com>
2021-02-07 19:14:14 -0800

    update deploy.sh for new docker containers

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 set -euf -o pipefail
 
 docker build -t divisordb -f docker/divisordb.Dockerfile .
-docker build -t divisorsearch -f docker/divisorsearch.Dockerfile .
+docker build -t generate -f docker/generate_search_blocks.Dockerfile .
+docker build -t process -f docker/process_search_blocks.Dockerfile .
 
 docker run -d --name divisordb -p 5432:5432 --memory="1G" divisordb:latest
 
@@ -11,4 +12,6 @@ export PGHOST=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" divisordb)
 
 # give time for the divisordb container to start up
 sleep 5
-docker run -d --name divisorsearch --env PGHOST="$PGHOST" --memory="15G" divisorsearch:latest
+docker run -d --name generate --env PGHOST="$PGHOST" --memory="1G" generate:latest
+sleep 5
+docker run -d --name process --env PGHOST="$PGHOST" --memory="1G" process:latest
