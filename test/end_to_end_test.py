0483042eeae3a19a88f167bc01d24824e644bdea
Jeremy Kun <j2kun@users.noreply.github.com>
2021-02-17 21:06:17 -0800

    extract ErringSearchStrategy for resuse in other tests

--- test/end_to_end_test.py
+++ test/end_to_end_test.py
@@ -37,6 +37,26 @@ class FakeArgs:
     refresh_period_seconds: int = 3
 
 
+def make_processor_test_strategy(process_block_fn):
+    class TestStrategy(SearchStrategy):
+        def index_name(self) -> str:
+            return SuperabundantSearchStrategy().index_name()
+
+        def starting_from(self, search_index: SearchIndex) -> SearchStrategy:
+            return self
+
+        def generate_search_blocks(self, count: int, batch_size: int) -> List[SearchMetadata]:
+            raise Exception('Failing for a test!')
+
+        def process_block(self, block: SearchMetadata) -> List[RiemannDivisorSum]:
+            process_block_fn()
+
+        def max(self, blocks: List[SearchIndex]) -> SearchIndex:
+            raise Exception('Failing for a test!')
+
+    return TestStrategy()
+
+
 def test_generate_and_process_blocks_no_duplicates(db_factory):
     db = db_factory()
     # this ensures nobody removes this attribute without updating this test
@@ -147,25 +167,14 @@ def test_mark_blocks_failed_when_block_computation_errs(db_factory):
             args=FakeArgs()
         )
 
-    class ErringSearchStrategy(SearchStrategy):
-        def index_name(self) -> str:
-            return SuperabundantSearchStrategy().index_name()
-
-        def starting_from(self, search_index: SearchIndex) -> SearchStrategy:
-            return self
-
-        def generate_search_blocks(self, count: int, batch_size: int) -> List[SearchMetadata]:
-            raise Exception('Failing for a test!')
-
-        def process_block(self, block: SearchMetadata) -> List[RiemannDivisorSum]:
-            raise Exception('Failing for a test!')
-
-        def max(self, blocks: List[SearchIndex]) -> SearchIndex:
-            raise Exception('Failing for a test!')
+    def raise_fn():
+        raise Exception('Failing for a test!')
 
     def run_processor():
         process_search_blocks.main(
-            db_factory(), search_strategy=ErringSearchStrategy())
+            db_factory(),
+            search_strategy=make_processor_test_strategy(raise_fn)
+        )
 
     p1 = Process(target=run_generator)
     p1.start()
@@ -180,5 +189,6 @@ def test_mark_blocks_failed_when_block_computation_errs(db_factory):
     p2.close()
 
     metadatas = db.load_metadata()
-    failed_blocks = [x for x in metadatas if x.state == SearchBlockState.FAILED]
+    failed_blocks = [x for x in metadatas if x.state ==
+                     SearchBlockState.FAILED]
     assert len(failed_blocks) > 0
