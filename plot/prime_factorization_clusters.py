0a3d2170999cb3dc08ef231a8fe9b23ef03fffa1
Jeremy Kun <j2kun@users.noreply.github.com>
2021-05-26 20:24:43 -0700

    add a better 'normalize_all' plot

--- plot/prime_factorization_clusters.py
+++ plot/prime_factorization_clusters.py
@@ -22,8 +22,10 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     df = pd.read_csv(args.divisor_sums_csv_path)
-    df = df.loc[df['witness_value'] >= 1.77]
+    df = df.loc[df['witness_value'] >= 1.69]
     df[['witness_value']] = StandardScaler().fit_transform(df[['witness_value']])
+    # to normalize all columns, use this instead of the previous line
+    # df[df.columns.values.tolist()] = StandardScaler().fit_transform(df[df.columns.values.tolist()])
     print(df.describe())
 
     if os.path.isfile(args.embedding_path):
@@ -32,7 +34,6 @@ if __name__ == "__main__":
     else:
         df = df.drop('log_n', axis=1)
         df = df.loc[:, (df != 0).any(axis=0)]
-        # normalized_data = StandardScaler().fit_transform(df)
         reducer = umap.UMAP()
         embedding = reducer.fit_transform(df.values)
         print(embedding.shape)
@@ -47,6 +48,7 @@ if __name__ == "__main__":
         s=1)
     plt.gca().set_aspect('equal', 'datalim')
     plt.colorbar(sc)
-    plt.title('UMAP projection of factorization of RH counterexample candidates', fontsize=12)
+    plt.title(
+        'UMAP projection of factorization of RH counterexample candidates', fontsize=12)
 
     plt.show()
